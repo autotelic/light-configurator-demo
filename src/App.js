1d5eb8c5803c03fa77e23cb3a0935805561a42a2
Edward Stone <ed@autotelic.com>
2020-08-27 15:12:53 -0700

    simple demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import classnames from 'classnames'
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('YOUR TEXT');
+  const [fontSize, setFontSize] = useState('big')
+  const [color, setColor] = useState('red')
+  const [flicker, setFlicker] = useState('none')
+  const [classes, setClasses] = useState(['text', fontSize, color, flicker])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="main">
+        <span className={classnames(classes)}>{text}</span>
+      </div>
+      <div className="control">
+        <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        <select
+          value={fontSize}
+          onChange={e => {
+            setFontSize(e.target.value)
+            setClasses(['text', e.target.value, color, flicker])
+          }}
+        >
+          {['big', 'medium', 'small'].map(size => (
+            <option
+              key={size}
+              value={size}
+            >
+              {size}
+            </option>
+          ))}
+        </select>
+        <select
+          value={color}
+          onChange={e => {
+            setColor(e.target.value)
+            setClasses(['text', fontSize, e.target.value, flicker])
+          }}
+        >
+          {['red', 'blue'].map(color => (
+            <option
+              key={color}
+              value={color}
+            >
+              {color}
+            </option>
+          ))}
+        </select>
+        <select
+          value={flicker}
+          onChange={e => {
+            setFlicker(e.target.value)
+            setClasses(['text', fontSize, color, e.target.value])
+          }}
+        >
+          {['none', 'quick', 'slow'].map(flicker => (
+            <option
+              key={flicker}
+              value={flicker}
+            >
+              {flicker}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
